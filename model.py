eef077fe475b054b964ad57a79dd35eb44792fb5
Michael Jaskiewicz <michael.jaskiewicz@snapsheet.me>
2018-11-19 22:26:28 -0600

    working junk model

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization, Dense, Flatten
 from keras.losses import mean_squared_error
 from keras.metrics import binary_accuracy
 from keras.optimizers import RMSprop
@@ -19,9 +19,12 @@ def model_1(input_shape=(128, 128, 3), num_classes=2, kernel_size=3, number_of_b
         prev_layer = maxp
         filters *= 2
 
-    classify = Conv2D(num_classes, (1, 1), activation='sigmoid')(up1)
+    #classify = Conv2D(num_classes, (1, 1), activation='sigmoid')(prev_layer)
+    flatten = Flatten()(prev_layer)
+    classify = Dense(num_classes, activation='sigmoid')(flatten)
     model = Model(inputs=inputs, outputs=classify)
     model.compile(optimizer=RMSprop(lr=0.0001), loss=mean_squared_error, metrics=[binary_accuracy])
+    import pdb; pdb.set_trace()
     return model
 
 
--- train.py
+++ train.py
@@ -1,25 +1,51 @@
 import keras.callbacks as keras_callbacks
 import sklearn.model_selection as sk_model_selection
+from os import listdir
+from os.path import join
+import numpy as np
+import pdb
+import cv2
 
 from model import model_1
+from perturbations import perturb
+from constants import DATA, BATCH_SIZE, TEST_SIZE, RANDOM_SEED, MODELS_FOLDER, EPOCHS
 
 
-EPOCHS = 64
-MODELS_FOLDER = 'models'
-BATCH_SIZE = 32
-RANDOM_SEED = 1
+def classification_vector(filename):
+    rv = np.zeros(2)
+    if 'cat' in filename:
+        rv[0] = 1
+    else:
+        rv[1] = 1
+    return rv
+
+
+def generator(files, image_directory, perturb_function=None):
+    while True:
+        for start in range(0, len(files), BATCH_SIZE):
+            x_batch = []
+            y_batch = []
+            end = min(start + BATCH_SIZE, len(files))
+            ids_train_batch = files[start:end]
+            for id in ids_train_batch:
+                img = cv2.imread(join(image_directory, id))
+
+                if perturb_function is not None:
+                    img = perturb_function(img)
+
+                x_batch.append(img)
+                y_batch.append(classification_vector(id))
+
+            x_batch = np.array(x_batch, np.float32) / 255
+            y_batch = np.array(y_batch, np.float32)
+
+            yield x_batch, y_batch
 
 
 def train():
 
-    files_train_split, files_valid_split = sk_model_selection.train_test_split(image_files,
-                                                                               test_size=valid_split + test_split,
-                                                                               random_state=RANDOM_SEED)
-    files_valid_split, files_test_split = sk_model_selection.train_test_split(files_valid_split,
-                                                                              test_size=test_split / (
-                                                                                          test_split + valid_split),
-                                                                              random_state=RANDOM_SEED)
-
+    files = listdir(DATA)
+    train_files, valid_files = sk_model_selection.train_test_split(files, test_size=TEST_SIZE, random_state=RANDOM_SEED)
 
     basic_model = model_1()
 
@@ -33,19 +59,22 @@ def train():
                                                    verbose=1,
                                                    epsilon=1e-4),
                  keras_callbacks.ModelCheckpoint(monitor='val_loss',
-                                                 filepath=os.path.join(MODELS_FOLDER,
-                                                                       'weights-{epoch:04d}-{val_dice_coeff:.2f}.hdf5'),
+                                                 filepath=join(MODELS_FOLDER,
+                                                               'weights-{epoch:04d}-{val_dice_coeff:.2f}.hdf5'),
                                                  save_best_only=True,
                                                  save_weights_only=True)]
 
-    model.fit_generator(
-        train_generator(files_train_split, image_directory, mask_directory),
-        steps_per_epoch=np.ceil(float(num_train_images) / float(BATCH_SIZE)),
+    basic_model.fit_generator(
+        generator(train_files, DATA, perturb),
+        steps_per_epoch=np.ceil(float(len(train_files)) / float(BATCH_SIZE)),
         epochs=EPOCHS,
         verbose=1,
         callbacks=callbacks,
-        validation_data=valid_generator(files_valid_split, image_directory, mask_directory),
-        validation_steps=np.ceil(float(num_valid_images) / float(BATCH_SIZE)))
+        validation_data=generator(valid_files, DATA),
+        validation_steps=np.ceil(float(len(valid_files)) / float(BATCH_SIZE)))
+
+
+
 
 
 train()
