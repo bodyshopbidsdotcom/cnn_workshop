736887669daefaba6fffc48f74fec68ae91a1301
Michael Jaskiewicz <michael.jaskiewicz@snapsheet.me>
2018-12-09 21:14:17 -0600

    x

--- app/chop.py
+++ app/chop.py
@@ -1,15 +1,19 @@
 import cv2
+from os import listdir
 from os.path import join
 from keras.models import Model
 import numpy as np
 import pdb
 from imutils import build_montages
+from sklearn.cluster import KMeans
 
 from model import model_1, model_2, model_alexnet
 
 #IMAGE = 'cat.93.jpg'
 #IMAGE = 'slovenia.png'
-IMAGE = 'michael.png'
+#IMAGE = 'michael.png'
+INPUT_SHAPE = (128, 128)
+PREDICTED_SHAPE = (8, 8)
 
 
 def build_bottleneck_model(model, layer_name):
@@ -25,32 +29,51 @@ def build_bottleneck_model(model, layer_name):
     return bottleneck_model
 
 
+def kmeans(input_data):
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(input_data)
+    labels = kmeans.labels_
+    score = metrics.silhouette_score(input_data, labels, metric='euclidean')
+    print(score)
+
+
+def generate_prediction(image_path, model):
+    image = cv2.imread(image_path)
+    image = np.array(image, np.float32) / 255
+    image = cv2.resize(image, INPUT_SHAPE)
+    image = np.expand_dims(image, axis=0)
+
+    predictions = model.predict(image)
+
+    num_predictions = predictions.shape[3]
+
+    predictions = predictions * 255
+    predictions = predictions.astype('uint8')
+    predictions = np.dsplit(predictions[0], num_predictions)  # Shape is (1, <width>, <height>, <depth>). Need to remove the leading 1.
+    zero_stack = np.zeros(PREDICTED_SHAPE)
+    predictions = [np.dstack((np.squeeze(prediction), zero_stack, zero_stack)) for prediction in predictions]
+    return predictions
+
+
 alex, name = model_alexnet(input_shape=(128, 128, 3))
 alex.summary()
 alex.load_weights('weights-0032-0.92.hdf5')
-target_layer = 'conv2d_1'
-#target_layer = 'max_pooling2d_1'
+target_layer = 'max_pooling2d_4'
 
 model = build_bottleneck_model(alex, target_layer)
 
-image_path = join('data', IMAGE)
-image = cv2.imread(image_path)
-image = np.array(image, np.float32) / 255
-image = cv2.resize(image, (128, 128))
-image = np.expand_dims(image, axis=0)
+features = []
+count = 0
+images = listdir('data')
+for image_filename in images:
+    image_path = join('data', image_filename)
 
-predictions = model.predict(image)
+    predictions = generate_prediction(image_path, model)
+    pdb.set_trace()
+    print('Predicted {}'.format(count))
+    count += 1
+    features.append(predictions)
 
-target_shape = (128, 128)
-#target_shape = (64, 64)
-num_predictions = predictions.shape[3]
-
-predictions = predictions * 255
-predictions = predictions.astype('uint8')
-predictions = np.dsplit(predictions[0], num_predictions)  # Shape is (1, <width>, <height>, <depth>). Need to remove the leading 1.
-zero_stack = np.zeros(target_shape)
-predictions = [np.dstack((np.squeeze(prediction), zero_stack, zero_stack)) for prediction in predictions]
-
+'''
 montage_shape = (128, 128)
 montages = build_montages(predictions, montage_shape, (7, 7))
 
@@ -58,4 +81,5 @@ for montage in montages:
     cv2.imshow(IMAGE, montage)
     if cv2.waitKey() == 113:
         break
+'''
 
