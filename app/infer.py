a85faa909699e35efa5a554e1d0ad722cc8b3dc2
Michael Jaskiewicz <michael.jaskiewicz@snapsheet.me>
2019-01-15 16:39:26 -0600

    confusion matrix

--- app/infer.py
+++ app/infer.py
@@ -6,25 +6,48 @@ import cv2
 import numpy as np
 
 from model import model_alexnet, model_inception_v3
-from constants import IMAGE_FILE_SUFFIXES
+from constants import IMAGE_FILE_SUFFIXES, TESTING
 
 
 MODEL = 'weights-0016-0.36.hdf5'
 
 
-def infer(model, image_array):
+def _infer(model, image_array):
     norm_image_array = np.array(image_array, np.float32) / 255
     prediction = model.predict(norm_image_array)
     return prediction
 
 
+def confusion_matrix(results):
+    '''
+                Predicted
+                yes  no
+    Actual yes  0    0
+           no   0    0
+    '''
+
+    matrix = np.zeros((2, 2))
+    for filename, prediction in results.items():
+        if 'yes' in filename:
+            if prediction[0] > prediction[1]:
+                matrix[0, 0] += 1 # actual yes, predicted yes
+            else:
+                matrix[1, 0] += 1 # actual yes, predicted no
+        else:
+            if prediction[0] > prediction[1]:
+                matrix[0, 1] += 1 # actual no, predicted yes
+            else:
+                matrix[1, 1] += 1 # actual no, predicted no
+
 
 model, name = model_inception_v3(input_shape=(256, 256, 3))
 model.load_weights(MODEL)
 
-folder = '/Users/jaskiemr/Downloads'
+folder = TESTING
 files = listdir(folder)
 files = [file for file in files if file.lower().endswith(tuple(IMAGE_FILE_SUFFIXES))]
+
+results = {}
 
 for file in files:
   image_path = join(folder, file)
@@ -33,7 +56,6 @@ for file in files:
   image_array = np.empty([1, 256, 256, 3])
   image_array[0] = image
 
-  pdb.set_trace()
   prediction = infer(model, image_array)
-
+  results['file'] = prediction
   print(prediction)
